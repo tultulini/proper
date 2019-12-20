ac271c1d0f10ba7fd6de0786d200f8a099ad4b4b
talfisher <talfisher@deloitte.ca>
2019-12-20 16:18:16 -0400

    changed package name

--- src/setup-package.js
+++ src/setup-package.js
@@ -27,11 +27,12 @@ function main() {
     if (sourceObj.main.startsWith("dist/")) {
         sourceObj.main = sourceObj.main.slice(5);
     }
-    
+
     fs.writeFileSync(__dirname + "/package.json", Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8") );
     fs.writeFileSync(__dirname + "/version.txt", Buffer.from(sourceObj.version, "utf-8") );
 
     fs.copyFileSync(__dirname + "/../.npmignore", __dirname + "/.npmignore");
+    fs.copyFileSync(__dirname + "/../README.md", __dirname + "/README.md");
 }
 
 main();
